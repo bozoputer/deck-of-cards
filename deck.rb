d53f0f46e2b72ac13f2abe3c422adfc877afe89b
bozoputer <e@ekathuria.dev>
2022-07-11 00:51:20 -0400

    Implement initialize method

--- deck.rb
+++ deck.rb
@@ -4,11 +4,23 @@ class Deck
   # a deck has 4 suits: Clubs Diamonds Hearts Spades
   # each suit has 13 ranks: 2 3 4 5 6 7 8 9 10, J, Q, K, A
 
+  # create a new deck of cards
   def initialize
+    # create an array of cards by combining the 4 suits with the 13 ranks
+    cards = []
+    suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    suits.each do |suit|
+      ranks.each do |rank|
+        cards << Card.new(rank, suit).description
+      end
+    end
+    @cards = cards
   end
 
   def draw
     # get the next card and remove it from the deck
+
   end
 
   def shuffle
@@ -19,3 +31,5 @@ class Deck
     # see the next card, but don't discard it
   end
 end
+
+deck = Deck.new
