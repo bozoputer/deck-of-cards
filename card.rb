fda99a285fd4dcabed48722f70fa9945d0cea7c0
bozoputer <e@ekathuria.dev>
2022-07-11 00:49:22 -0400

    Implement methods for Card class

--- card.rb
+++ card.rb
@@ -2,10 +2,13 @@ class Card
   attr_accessor :rank, :suit
 
   def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
   end
 
   def description
     # describe the card
     # e.g. A of spades
+    "#{rank} of #{suit}"
   end
 end
